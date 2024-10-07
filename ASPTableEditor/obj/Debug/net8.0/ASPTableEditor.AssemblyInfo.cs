bedaafa67217a663e353fb951b52cc520fcf4aac
HappyUnknown <danchenkov.vladyslav@gmail.com>
2024-10-07 14:01:54 +0300

    Alert instead of label change

--- ASPTableEditor/obj/Debug/net8.0/ASPTableEditor.AssemblyInfo.cs
+++ ASPTableEditor/obj/Debug/net8.0/ASPTableEditor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54e591ed834fb443e9612b1e047737f5dc450c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
