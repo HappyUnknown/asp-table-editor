1b187ad721a8ba2fbede4583af0a97b761219729
HappyUnknown <danchenkov.vladyslav@gmail.com>
2024-10-07 18:29:23 +0300

    Home buttons in CRUD

--- ASPTableEditor/obj/Debug/net8.0/ASPTableEditor.AssemblyInfo.cs
+++ ASPTableEditor/obj/Debug/net8.0/ASPTableEditor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+186d2bc7d51b455985218ec68d33cff09c9685cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b59a02d742eddd560dd7de08292a5d28ecd7583")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
