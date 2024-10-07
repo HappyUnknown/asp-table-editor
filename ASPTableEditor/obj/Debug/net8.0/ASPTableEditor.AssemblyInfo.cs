21d131a8d5a3d64a4e1b9b2130f99183c2b86592
HappyUnknown <danchenkov.vladyslav@gmail.com>
2024-10-07 21:57:23 +0300

    Button for listing Employees back again

--- ASPTableEditor/obj/Debug/net8.0/ASPTableEditor.AssemblyInfo.cs
+++ ASPTableEditor/obj/Debug/net8.0/ASPTableEditor.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d0e6830276a11ca3b867dfe6f79da3f82f2f65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e7ec9cf1619e64a3287579869c1bb5800e0aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPTableEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
