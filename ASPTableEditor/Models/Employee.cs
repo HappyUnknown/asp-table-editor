a4cce1e23a5a1008f2a3f29d49a760c59d61484c
HappyUnknown <danchenkov.vladyslav@gmail.com>
2024-10-08 10:27:39 +0300

    Creation validated

--- ASPTableEditor/Models/Employee.cs
+++ ASPTableEditor/Models/Employee.cs
@@ -1,12 +1,23 @@
-﻿namespace ASPTableEditor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPTableEditor.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Birth Date is required")]
         public DateTime BirthDate { get; set; }
         public bool IsMarried { get; set; }
+
+        [Phone(ErrorMessage = "Invalid phone number")]
         public string Phone { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Salary must be a positive number")]
         public int Salary { get; set; }
         public Employee()
         {
